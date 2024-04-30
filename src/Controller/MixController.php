bad3bf31916a1867f299efa1413ae5e4acf41390
amal5550 <sloumaamal65@gmail.com>
2024-04-30 11:41:51 +0100

    Querying for a Single Entity for a "Show" Page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -1,15 +1,13 @@
 <?php
 namespace App\Controller;
-
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 class MixController extends AbstractController
 {
-   
     #[Route('/mix/new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
@@ -20,14 +18,23 @@ class MixController extends AbstractController
         $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
-
         $entityManager->persist($mix);
         $entityManager->flush();
-        
         return new Response(sprintf(
             'Mix %d is %d tracks of pure 80\'s heaven',
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        if (!$mix) {
+            throw $this->createNotFoundException('Mix not found');
+        }
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
